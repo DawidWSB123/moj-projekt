e90175b90f5da00da9667033556a66dad294a426
DawidWSB123 <wrx97092@student.wroclaw.merito.pl>
2025-07-04 20:37:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,54 @@
 tasks = []
 
+def show_menu():
+    print("\n--- TO DO APP ---")
+    print("1. Dodaj zadanie")
+    print("2. Pokaż zadania")
+    print("3. Usuń zadanie")
+    print("4. Wyjście")
+
+def add_task():
+    task = input("Wpisz nowe zadanie: ")
+    tasks.append(task)
+    print("Dodano zadanie.")
+
 def show_tasks():
     if not tasks:
         print("Brak zadań.")
     else:
         print("Twoje zadania:")
-        for i, task in enumerate(tasks, 1):
+        for i, task in enumerate(tasks, start=1):
             print(f"{i}. {task}")
-
-def add_task():
-    task = input("Podaj nowe zadanie: ")
-    tasks.append(task)
-    print("Dodano zadanie.")
 
 def remove_task():
     show_tasks()
-    if tasks:
-        try:
-            num = int(input("Podaj numer zadania do usunięcia: "))
-            if 1 <= num <= len(tasks):
-                removed = tasks.pop(num-1)
-                print(f"Usunięto zadanie: {removed}")
-            else:
-                print("Niepoprawny numer.")
-        except ValueError:
-            print("To nie jest numer.")
+    if not tasks:
+        return
+    try:
+        task_num = int(input("Podaj numer zadania do usunięcia: "))
+        if 1 <= task_num <= len(tasks):
+            removed = tasks.pop(task_num - 1)
+            print(f"Usunięto zadanie: {removed}")
+        else:
+            print("Nieprawidłowy numer.")
+    except ValueError:
+        print("Wprowadź poprawny numer.")
 
 def main():
     while True:
-        print("\nCo chcesz zrobić?")
-        print("1. Pokaż zadania")
-        print("2. Dodaj zadanie")
-        print("3. Usuń zadanie")
-        print("4. Wyjdź")
+        show_menu()
         choice = input("Wybierz opcję: ")
-        if choice == "1":
-            show_tasks()
-        elif choice == "2":
+        if choice == '1':
             add_task()
-        elif choice == "3":
+        elif choice == '2':
+            show_tasks()
+        elif choice == '3':
             remove_task()
-        elif choice == "4":
+        elif choice == '4':
             print("Do widzenia!")
             break
         else:
-            print("Nieznana opcja.")
+            print("Nieznana opcja, spróbuj jeszcze raz.")
 
 if __name__ == "__main__":
     main()
